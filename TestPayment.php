ddf8927b711b5636404a3be1d166d3e59427aefe
Omer <warlox414@gmail.com>
2016-08-16 12:08:47 +0400

    testing

--- TestPayment.php
+++ TestPayment.php
@@ -3,6 +3,15 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 echo 'hi there';
+
+$sandbox = true;
+$api_username = 'warlox414-merchant_api1.gmail.com';
+$api_password = 'Z9VFDKJA2FJQ5DZW';
+$api_signature = 'ALGGHNwYi1b3f.SAFEBGIVyGMKAwAxeOTUcB8qI8ZeSPXiP02XSlsQtG';
+$print_headers = true;
+$log_results = true;
+$log_path = '/log';
+
 // Create PayPal object.
 $PayPalConfig = array(
     'Sandbox' => $sandbox,
